eafe6b2b445bc57b5e8d9798d2cd486bd33daccd
Iker E <MY_NAME@example.com>
2026-01-21 17:13:30 +0100

    Cambio basico conexion bbdd

--- backend/bbdd.php
+++ backend/bbdd.php
@@ -1,16 +1,17 @@
 <?php
+    header('Content-Type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
     define('SERVIDOR', 'localhost');
     define('BBDD', 'gamefest');
     define('USUARIO', 'root');
     define('CLAVE', '12345');
     $conexion = new mysqli(SERVIDOR, USUARIO, null, BBDD);
-
     $conexion->set_charset('utf8mb4');
     $query="SELECT * FROM games";
     $resultado = $conexion->query($query);
-    while($fila=$resultado->fetch_assoc()){
-      print "<p>$fila[titulo]</p>\n";
-    }
+
+    $dato=$resultado->fetch_all();
+    print json_encode($dato, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     $resultado->free();
     $conexion->close();
 ?>
