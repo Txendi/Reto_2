48c5799258a1e541b0d8a5be9ddf0304ec844bac
Endika <endika.mateosbe@elorrieta-errekamari.com>
2026-01-27 19:28:17 +0100

    Pagina view funcionando correctamente y bbdd.php funcionando correctamente

--- backend/bbdd.php
+++ backend/bbdd.php
@@ -9,18 +9,18 @@
     $conexion->set_charset('utf8mb4');
     
 
-    function filter_juegos(array $canciones, string $query): array {
+    function filter_juegos(array $juegos, string $query): array {
         $result = [];
         $query = trim($query);
 
-        foreach ($canciones as $c) {
-            $c["plataformas"] = json_decode( $c["plataformas"]);
+        foreach ($juegos as $j) {
+            $j["plataformas"] = json_decode( $j["plataformas"]);
             if ($query !== "") {
-                $inTitulo  = strtolower(stripos($c["titulo"], $query));
-                $inGenero = strtolower(stripos($c["genero"], $query));
+                $inTitulo  = stripos($j["titulo"], $query);
+                $inGenero = stripos($j["genero"], $query);
                 $inPlataforma = false;
-                foreach($c["plataformas"] as $plat){
-                    $inPlataforma = strtolower(stripos($plat, $query));
+                foreach($j["plataformas"] as $plat){
+                    $inPlataforma = stripos($plat, $query);
                     if($inPlataforma !== false){
                         break;
                     }
@@ -29,7 +29,7 @@
                     continue;
                 }
             }
-            $result[] = $c;
+            $result[] = $j;
         }
         return $result;
     }
@@ -68,6 +68,14 @@
         $array = filter_eventos( $eventos,  $_GET["tipo"] ?? "",  $_GET["fecha"] ?? "",  $_GET["plazas"] ?? "");
         print json_encode([ceil(count($array)/9), array_slice($array, $_GET["pagina"] *9, 9)]);
         $resultado->free();
+
+        //filter_eventos();
+    }else if($action === "numeroPaginas"){
+        $query = "SELECT COUNT(*) FROM events";
+        $resultado = $conexion->query($query);
+        print $resultado->fetch_all(MYSQLI_ASSOC )/9;
+        $resultado->free();
+
     }else if($action === "registrarse"){
         $query = "INSERT INTO users VALUES ({$_GET['usuario']}, {$_GET['email']}, {$_GET['contrasena']}, USER, now())";
          $conexion->query($query);
