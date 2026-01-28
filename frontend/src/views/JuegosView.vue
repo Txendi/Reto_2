da9160a52ab3d26f4d9c9e048af76f8719ef434c
Iker E <MY_NAME@example.com>
2026-01-28 17:11:20 +0100

    Docker cambios

--- frontend/src/views/JuegosView.vue
+++ frontend/src/views/JuegosView.vue
@@ -1,71 +1,67 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, watch, reactive } from 'vue'
 
-const api = 'http://localhost/bbdd.php'
+//-> Es como que se ejecuta de nuevo (se monta encima del componente)
+//-> Reacciona a los cambios de la variable
 
-const juegos = ref([])
-const cargando = ref(true)
-const error = ref('')
+const api = 'http://localhost/bbdd.php?action=listaJuegos'
+
+//-> La lista de los juegos
+const juegos = reactive({array: []})
+
+const cargando = ref(false) //-> Para el mensaje de cuando cargue los juegos,
+const error = ref('')      // |_> A su vez mostrara el mensaje de error si no los encuentra
+
+//-> Para el input
 const busqueda = ref('')
 
-const juegosFiltrados = computed(() => {
-  if (!busqueda.value) {
-    return juegos.value
-  }
-  
-  const texto = busqueda.value.toLowerCase()
+const fetchJuegos = async () => {
+  cargando.value = true;
+  error.value = '';
 
-  return juegos.value.filter((juego) => {
-    const titulo = juego.titulo?.toLowerCase() || ''
-    const genero = juego.genero?.toLowerCase() || ''
-    const plataformas = Array.isArray(juego.plataformas)
-      ? juego.plataformas.join(' ').toLowerCase()
-      : (juego.plataformas || '').toLowerCase()
-
-    return (
-      titulo.includes(texto) ||
-      genero.includes(texto) ||
-      plataformas.includes(texto)
-    )
-  })
-})
-
-onMounted(async () => {
   try {
-    const response = await fetch(api)
-    if (!response.ok) {
-      throw new Error('Error de HTTP: ' + response.status)
-    }
-    const data = await response.json()
-    juegos.value = data
+
+    const url = `${api}&q=${encodeURIComponent(busqueda.value)}`;  // el encode sirve para que no deje espacion ni huecos raros
+
+    const response = await fetch(url);
+    console.log(response)
+    if (!response.ok) throw new Error('Error HTTP ' + response.status); // Si no encuentra la URL
+    console.log(url)
+    juegos.array = await response.json();
+    console.log(juegos.array)
+
   } catch (e) {
-    error.value = 'No se ha podido cargar la pagina con los juegos'
+    console.log(e);
   } finally {
-    cargando.value = false
+    cargando.value = false;
   }
+}
+
+onMounted(() => {       // Cuando entre en la vista se va a poner a cargar los juegos
+  fetchJuegos()
+  
 })
+
+watch(busqueda, () => {  // Como el propio nombre dice, mira para ver que cada vez que la variable busqueda (el input)
+  fetchJuegos()          // va cambiando mientras escribe, y se actualiza a la vez 
+  console.log(23)
+})
+
 </script>
 
 <template>
   <section class="max-w-7xl mx-auto px-4 py-8">
     <h1 class="text-3xl font-bold mb-5">Lista de Videojuegos</h1>
 
-    <input type="text" placeholder="Buscar juegos..." class="mb-5 p-2 border-2 rounded-xl w-full" v-model="busqueda"/>
+    <input type="text" placeholder="Buscar juegos..." class="mb-5 p-2 border-2 rounded-xl w-full" v-model="busqueda" />
 
     <p v-if="cargando" class="text-gray-500">Cargando...</p>
     <p v-else-if="error" class="text-red-600">{{ error }}</p>
 
     <div v-else class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-      <article
-        v-for="juego in juegosFiltrados"
-        :key="juego.id"
-        class="bg-gray-200 border-b-gray-800 rounded-lg shadow hover:shadow-xl cursor-pointer flex flex-col"
-      >
-        <img
-          :src="`/img/games/${juego.imagen}`"
-          :alt="juego.titulo"
-          class="w-full h-56 object-cover rounded-lg"
-        />
+      <article v-for="juego in juegos.array" :key="juego.id"
+        class="bg-gray-200 border-b-gray-800 rounded-lg shadow hover:shadow-xl cursor-pointer flex flex-col">
+        <img :src="`/img/games/${juego.imagen}`" :alt="juego.titulo" class="w-full h-56 object-cover rounded-lg" />
 
         <div class="p-4 flex flex-col gap-2 flex-grow">
           <h3 class="text-lg font-semibold">
@@ -92,5 +88,4 @@ onMounted(async () => {
   </section>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
