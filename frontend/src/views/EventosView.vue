b38bd323d75dd4f521e4f2a08ccfddce29e76ef9
Endika <endika.mateosbe@elorrieta-errekamari.com>
2026-01-29 15:53:00 +0100

    Develop copiado

--- frontend/src/views/EventosView.vue
+++ frontend/src/views/EventosView.vue
@@ -1,132 +1,114 @@
 <script setup>
-import { ref, computed, onMounted, watch } from 'vue'
+import { ref, computed, onMounted } from 'vue';
 
-const eventos = ref([])
-const cargando = ref(true)
-const error = ref('')
-const paginaActual = ref(0)
-const tipoSeleccionado = ref('')
+const eventos = ref([]);
+const cargando = ref(true);
+const error = ref('');
+const tipoSeleccionado = ref('');
+const paginaActual = ref(0);
+const totalPaginas = ref(0)
 
-const plazasDisponibles = ref(false) //
-const fechaSeleccionada = ref('')
+
 
 const cargarEventos = async () => {
-  try {
-    const response = await fetch(
-      `http://localhost/bbdd.php?action=listaEventos&pagina=${paginaActual.value}&tipo=${tipoSeleccionado.value}&fecha=${fechaSeleccionada.value}&plazasLibres  =${plazasDisponibles.value}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: JSON.stringify({
-          title: 'Mi nuevo post',
-          body: 'Contenido del post',
-          userId: 1,
-        }),
-      }
-    )
+    try {
+        const response = await fetch(`http://localhost/bbdd.php?action=listaEventos&pagina=${paginaActual.value}`)
+        if (!response.ok) {
+            throw new Error('Error de HTTP: ' + response.status)
+        }
+        const data = await response.json()
+        console.log(data)  // así puedes ver exactamente qué devuelve tu PHP
+        totalPaginas.value = data[0]
+        eventos.value = data[1]
 
-    const data = await response.json()
-    console.log(data) // así puedes ver exactamente qué devuelve tu PHP
-    eventos.value = data[1]
-  } catch (e) {
-    error.value = 'No se ha podido cargar la pagina con los eventos'
-  } finally {
-    cargando.value = false
-  }
+
+    } catch (e) {
+        e.value = 'No se ha podido cargar la pagina con los eventos'
+    } finally {
+        cargando.value = false
+    }
 }
+
 
 onMounted(() => {
-  cargarEventos()
+    cargarEventos()
 })
 
-watch(
-  (tipoSeleccionado,
-  plazasDisponibles,
-  () => {
-    cargarEventos()
-  })
-)
-
 const cambiarPagina = (numPagina) => {
-  paginaActual.value = numPagina
-  cargarEventos()
+    paginaActual.value = numPagina;
+    cargarEventos();
 }
+
 </script>
 
 <template>
-  <section class="max-w-7xl mx-auto px-4 pt-8 pb-25">
-    <h1 class="text-3xl text-white font-bold mb-5">Lista de Eventos</h1>
 
-    <div class="flex items-center gap-6 mb-5">
-      <select v-model="tipoSeleccionado" class="text-black p-2 border-2 rounded-xl w-3xl">
-        <option value="">Tipos</option>
-        <option v-for="evento in eventos" :key="evento.tipo" :value="evento.tipo">
-          {{ evento.tipo }}
-        </option>
-      </select>
+    <section class="max-w-7xl mx-auto px-4 pt-8 pb-25">
+        <h1 class="text-3xl text-white font-bold mb-5">Lista de Eventos</h1>
 
-      <label class="flex items-center gap-2 text-white">
-        <input type="checkbox" v-model="plazasDisponibles" />
-        Solo con plazas disponibles
-      </label>
-    </div>
 
-    <p v-if="cargando" class="text-gray-500">Cargando...</p>
-    <p v-else-if="error" class="text-red-600">{{ error }}</p>
+        <select v-model="tipoSeleccionado" class="text-black mb-5 p-2 border-2 rounded-xl w-3xl">
 
-    <div v-else class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-      <article
-        v-for="evento in eventos"
-        :key="evento.id"
-        class="bg-gray-200 border-b-gray-800 rounded-lg shadow hover:shadow-xl cursor-pointer flex flex-col"
-      >
-        <img
-          :src="`/img/events/${evento.imagen}`"
-          :alt="evento.titulo"
-          class="w-full h-56 object-cover rounded-lg"
-        />
+            <option value="">Tipos</option>
 
-        <div class="p-4 flex flex-col gap-2 flex-grow">
-          <h3 class="text-lg font-semibold text-center">
-            {{ evento.titulo }}
-          </h3>
-          <div class="flex items-end gap-3">
-            <div class="h-full w-1/2">
-              <p class="text-sm text-gray-600 text-center">
+            <option v-for="evento in eventos" :key="evento.tipo" :value="evento.tipo">
                 {{ evento.tipo }}
-              </p>
-            </div>
-            <div class="h-full w-full justify-end">
-              <p class="text-sm text-gray-600 text-center">
-                <span class="font-bold">Plazas: </span>
-                {{ evento.plazasLibres }}
-              </p>
-            </div>
-          </div>
-          <p class="text-sm text-gray-600">
-            <span class="font-bold">Descripcion: </span>
-            {{ evento.descripcion }}
-          </p>
+            </option>
 
-          <div class="h-full flex justify-center items-end gap-3">
-            <p class="text-sm text-gray-700">
-              {{ evento.hora }}
-            </p>
-            <p class="text-sm text-gray-700">
-              {{ evento.fecha }}
-            </p>
-          </div>
+        </select>
+
+
+
+        <p v-if="cargando" class="text-gray-500">Cargando...</p>
+        <p v-else-if="error" class="text-red-600">{{ error }}</p>
+
+        <div v-else class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+            <article v-for="evento in eventos" :key="evento.id"
+                class="bg-gray-200 border-b-gray-800 rounded-lg shadow hover:shadow-xl cursor-pointer flex flex-col">
+                <img :src="`/img/events/${evento.imagen}`" :alt="evento.titulo"
+                    class="w-full h-56 object-cover rounded-lg" />
+
+                <div class="p-4 flex flex-col gap-2 flex-grow">
+                    <h3 class="text-lg font-semibold text-center">
+                        {{ evento.titulo }}
+                    </h3>
+                    <div class="flex items-end gap-3">
+                        <div class="h-full w-1/2 ">
+                            <p class="text-sm text-gray-600 text-center">
+                                {{ evento.tipo }}
+                            </p>
+                        </div>
+                        <div class="h-full w-full justify-end">
+                            <p class="text-sm text-gray-600 text-center">
+                                <span class="font-bold">Plazas: </span>
+                                {{ evento.plazasLibres }}
+                            </p>
+                        </div>
+
+                    </div>
+                    <p class="text-sm text-gray-600">
+                        <span class="font-bold">Descripcion: </span>
+                        {{ evento.descripcion }}
+                    </p>
+
+                    <div class="h-full flex justify-center items-end gap-3">
+                        <p class="text-sm text-gray-700 ">
+                            {{ evento.hora }}
+                        </p>
+                        <p class="text-sm text-gray-700 ">
+                            {{ evento.fecha }}
+                        </p>
+
+                    </div>
+                </div>
+            </article>
         </div>
-      </article>
-    </div>
-    <div class="flex justify-center mt-7 gap-8">
-      <button @click="cambiarPagina(0)" class="bg-yellow-200 px-5 py-2 cursor-pointer">1</button>
-      <button @click="cambiarPagina(1)" class="bg-yellow-200 px-5 py-2 cursor-pointer">2</button>
-      <button @click="cambiarPagina(2)" class="bg-yellow-200 px-5 py-2 cursor-pointer">3</button>
-    </div>
-  </section>
+        <div class="flex justify-center mt-7 gap-8">
+            <button class="bg-yellow-200 px-5 py-2 cursor-pointer hover:bg-white" v-for="n in totalPaginas" :key="n" @click="cambiarPagina(n - 1)">
+                {{ n }}
+            </button>
+        </div>
+    </section>
 </template>
 
 <style></style>
