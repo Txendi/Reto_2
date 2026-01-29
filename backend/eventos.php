a9050426c4578afc7f69fd7e6e391dfdb9b95d01
David Sanchez <david.sanchezva@elorrieta-errekamari.com>
2026-01-29 21:07:10 +0100

    actualizacion eventos

--- backend/eventos.php
+++ backend/eventos.php
@@ -2,8 +2,12 @@
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
 
-error_reporting(0);
-ini_set('display_errors', 0);
+/* error_reporting(0);
+ini_set('display_errors', 0); */
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 
 define('SERVIDOR', 'localhost');
 define('BBDD', 'gamefest');
