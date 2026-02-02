b254f879668da471e36a6f34194edb207334d0e2
Iker E <MY_NAME@example.com>
2026-02-02 17:05:52 +0100

    commit

--- frontend/src/stores/userStore.js
+++ frontend/src/stores/userStore.js
@@ -1,16 +1,6 @@
 //import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-function getCookie(cname) {
-  var name = cname + "="; //Crea una variable con el texto del campo y un  igual
-  var ca = document.cookie.split(';'); //divide la cookie en trozos separados  por ;
-  for(let i=0; i<ca.length; i++) { //Recorre cada trozo de la cookie  
-    let c = ca[i].trim(); //Extraemos la cookie y eliminamos el espacio inicial
-    if (c.indexOf(name) == 0) //Si encuentra el campo buscado
-      return c.substring(name.length,c.length); //Lo devuelve
-    }
-  return ""; //Si no devuelve vacío
-}
 
 
 export const useUserStore = defineStore('user', {
