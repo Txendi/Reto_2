fdbb35b9c45053d7e65250aec3fbfcbcc3c6f8bb
Iker E <MY_NAME@example.com>
2026-02-03 15:59:43 +0100

    dfgrgdssd

--- backend/juegos.php
+++ backend/juegos.php
@@ -1,6 +1,6 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
-header("Access-Control-Allow-Origin: *");
+//header('Content-Type: application/json; charset=utf-8');
+//header("Access-Control-Allow-Origin: *");
 define('SERVIDOR', 'mysql');
 define('BBDD', 'gamefest');
 define('USUARIO', 'root');
