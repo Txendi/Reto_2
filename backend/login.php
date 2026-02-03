2e4065bc9a5a15eb3d4ee086a480775eb9b494cd
Iker E <MY_NAME@example.com>
2026-02-03 16:56:48 +0100

    vdfadas

--- backend/login.php
+++ backend/login.php
@@ -4,9 +4,9 @@ session_start();
 error_reporting(0);
 ini_set('display_errors', 0);
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE");
+// header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header('Content-Type: application/json; charset=utf-8');
 
 // Configuración de BD
