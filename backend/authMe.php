a9ee1dfb02072d66726e0532c0e8269c1d44e49f
David Sanchez <david.sanchezva@elorrieta-errekamari.com>
2026-02-06 15:07:54 +0100

    arreglo errores

--- backend/authMe.php
+++ backend/authMe.php
@@ -3,12 +3,12 @@ session_start();
 
 require_once "conexion.php";
 
-// if (!isset($_SESSION['id'])) {
-//     echo json_encode([
-//         "authenticated" => false
-//     ]);
-//     exit;
-// }
+if (!isset($_SESSION['id'])) {
+    echo json_encode([
+        "authenticated" => false
+    ]);
+    exit;
+}
 
     $stmt = $conexion->prepare("SELECT id, username, email, 'role' FROM users WHERE id = ?");
     $stmt->bind_param("i", $_SESSION['id']);
